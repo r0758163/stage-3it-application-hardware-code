9d87cea2fde9ad97da6e175f380007487fbaec80
Gianni Rutten <45198573+r0758163@users.noreply.github.com>
2021-05-19 16:08:29 +0200

    Update config3.js

--- applicatie code/www/js/config3.js
+++ applicatie code/www/js/config3.js
@@ -1,7 +1,7 @@
 // @ts-check
 
 const config3 = {
-    endpoint: "https://cosmos-stage-gianni-serverless.documents.azure.com:443/",
+    endpoint: #endpoint,
     key: #primarykey,
     databaseId: "stage-gianni-cosmos",
     containerId: "Klanten",
